ae059dce9906e234cf9abc6137e4fec028a2049f
ginkul <geenkool@yandex.ru>
2024-02-10 12:58:06 +0300

    Move tokenize to another file

---
+++ lib/tokenizer.ex
@@ -0,0 +1,67 @@
+defmodule Tokenizer do
+  defmodule Token do
+    defstruct [:type, :value, :line, :left, :right] 
+
+    @type t :: %Token{
+      type: :word | :symbol,
+      value: String.t(),
+      line: integer(),
+      left: integer(),
+      right: integer(),
+    }
+  end
+
+  @doc """
+  This function takes code as input and converts it to a list of tokens.
+  Token represents the first layer of parsing code.
+  Some symbols, e.g. `*`, can have different meaning depending on the context.
+  Tokens are supposed to read them irregardless of their meaning, so `*` is just a symbol,
+  `select` is a word etc.
+  """
+  @spec tokenize(list(String.t()), integer(), integer()) :: list(Token.t())
+  def tokenize(str, line \\ 0, position \\ 0)
+  def tokenize([], _line, _position), do: []
+  def tokenize(str, line, position) do
+    IO.puts :stderr, str
+    token = get_token(str, line, position)
+    case token do
+      {:value, token} ->
+        new_position = token.right
+        new_str = Enum.drop(str, new_position - position)
+        [token | tokenize(new_str, token.line, new_position)]
+      {:space, nil} ->
+        tokenize(Enum.drop(str, 1), line, position + 1)
+      {:newline, nil} ->
+        tokenize(Enum.drop(str, 1), line + 1, position)
+      {:err, head} ->
+        IO.puts :stderr, "Couldn't parse an element: " <> head
+        []
+    end
+  end
+
+  @spec get_token(list(String.t()), integer(), integer()) :: tuple()
+  defp get_token(str, line, position) do
+    head = hd(str)
+    cond do
+      String.match?(head, ~r/^[a-z]$/i) -> {:value, get_token(str, line, position, :word)}
+      String.contains?(head, [".", ",", "*", "+", "-", "/", ";"]) -> {:value, get_token(str, line, position, :symbol)}
+      String.contains?(head, ["\n", "\r\n"]) -> {:newline, nil}
+      head == " " -> {:space, nil}
+      true -> {:err, head}
+    end
+  end
+
+  @spec get_token(list(String.t()), integer(), integer(), :word | :symbol) :: Token.t()
+  defp get_token(str, line, position, type)
+  defp get_token(str, line, position, :word) do
+    value = str
+    |> Enum.take_while(&String.match?(&1, ~r/^[a-z]$/i))
+    |> Enum.join
+
+    %Token{type: :word, value: value, line: line, left: position, right: position + String.length(value)}
+  end
+
+  defp get_token(str, line, position, :symbol) do
+    %Token{type: :symbol, value: hd(str), line: line, left: position, right: position + 1}
+  end
+end
